095d787ec20ffc489381250d5cc2b12d3fbda42a
ManganiBiagio <biagiomangani@live.it>
2023-02-13 15:44:50 +0100

    firts commit

---
+++ app/Http/Controllers/Admin/TypeController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTypeRequest;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $types=Type::all();
+        return view("admin.type.index",compact("types"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("admin.type.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTypeRequest $request)
+    {
+        $data=$request->validated();
+        $data=$request->all();
+
+        $type=Type::create($data);
+        $type->save();
+        return redirect()->route("admin.types.index");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    
+    {
+        
+        $id=(int) $request->type;
+        $request->input("destroyAnyway");
+        $destroyAnyway=$request->destroyAnyway;
+        
+        
+        $typeToDelete=Type::findOrFail($id);
+        $types=Type::all();
+        if(!$typeToDelete->projects->isEmpty()){
+            
+            if(is_null($destroyAnyway)){
+                return view("admin.type.index",[
+                    "types"=>$types,
+                    "typeToDelete"=>$typeToDelete,
+                    "msgError"=>"Stai cercando di cancellare una categoria associata a dei progetti esistenti"]);
+                    
+            }
+            else{
+                
+                // qui dobbiamo cancellare l'associazione con i progetti nel db
+                // $typeToDelete->delete();
+                 return redirect()->route("admin.types.index");
+            }
+        
+            }
+        $typeToDelete->delete();
+        return redirect()->route("admin.types.index");
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\TypeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Type;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PageController::class,"index"])->name("index");
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'verified'])
+    ->prefix("admin") // porzione di uri che verrà inserita prima di ogni rotta
+    ->name("admin.") // porzione di testo inserita prima del name di ogni rotta
+    ->group(function () {
+        Route::get('/dashboard', [DashboardController::class, "home"])->name('dashboard');
+        
+        
+
+        Route::resource("projects", ProjectController::class);
+       
+        Route::prefix("types")
+        ->name("types.")
+        ->group(function(){
+            Route::get("/",[TypeController::class,"index"])->name("index");
+            Route::get("/create",[TypeController::class,"create"])->name("create");
+            Route::post("/", [TypeController::class, "store"])->name("store");
+            Route::delete("/{type}", [TypeController::class, "destroy"])->name("destroy");
+
+        });
+       
+    });
+       
